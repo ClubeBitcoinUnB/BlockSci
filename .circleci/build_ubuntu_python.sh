3cb7a3f05a66627dcbc462d24ff1f201b22d5d85
yan <102800044+yan-pi@users.noreply.github.com>
2026-07-06 12:27:36 -0300

    ci: remove legacy circleci and travis configs

--- .circleci/build_ubuntu_python.sh
+++
@@ -1,4 +0,0 @@
-set -ev
-pip3 install -U pip setuptools
-pip3 install pytest pytest-regtest pytz tzlocal pytest-benchmark
-pip3 install sphinx sphinx_rtd_theme
--- .circleci/compile_ubuntu_python.sh
+++
@@ -1,3 +0,0 @@
-set -ev
-sed -i 's/j4/j2/g' blockscipy/setup.py
-CC=gcc-7 CXX=g++-7 pip3 install -v -e blockscipy
